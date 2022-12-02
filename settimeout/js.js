452471d7c0560d6186cccf7999cda4a218296e26
NguyenQuocTrung20 <qtvn2323@gmail.com>
2022-12-02 20:03:19 +0700

    lab5

---
+++ settimeout/js.js
@@ -0,0 +1,27 @@
+let FirstLine = document.getElementById('first')
+let SecondLine = document.getElementById('second')
+let ThirdLine = document.getElementById('third')
+let FouthLine = document.getElementById('fouth')
+let FithLine = document.getElementById('fith')
+let SixLine = document.getElementById('six')
+
+setTimeout(() => {
+    FirstLine = document.getElementById('first').style.visibility = "visible"
+}, 2000);
+
+setTimeout(() => {
+    SecondLine = document.getElementById('second').style.visibility = "visible"
+}, 3500);
+
+setTimeout(() => {
+    ThirdLine = document.getElementById('third').style.visibility = "visible"
+}, 4500);
+setTimeout(() => {
+    FouthLine = document.getElementById('fouth').style.visibility = "visible"
+}, 6800);
+setTimeout(() => {
+    FithLine = document.getElementById('fith').style.visibility = "visible"
+}, 8500);
+setTimeout(() => {
+    SixLine = document.getElementById('six').style.visibility = "visible"
+}, 9600);
